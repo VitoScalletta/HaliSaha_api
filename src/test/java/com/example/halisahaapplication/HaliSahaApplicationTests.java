6a3ebd0960044327eb4151dfd16a60611a29efe1
VitoScalletta <muhammetemre3737@gmail.com>
2026-03-09 09:45:41 +0300

    Faz 0 Proje oluşturuldu Senrkonizasyon Test ediliyor

---
+++ src/test/java/com/example/halisahaapplication/HaliSahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halisahaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaliSahaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
