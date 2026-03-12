54960b73a5b8f06c3d37383f45ebc7c2cb50f108
VitoScalletta <muhammetemre3737@gmail.com>
2026-03-12 14:58:16 +0300

    faz 4'e başlandı Controller sınıfları oluşturuldu CRUD endpointleri yazıldı

---
+++ src/main/java/com/example/halisahaapplication/controller/MatchController.java
@@ -0,0 +1,51 @@
+package com.example.halisahaapplication.controller;
+
+import com.example.halisahaapplication.dto.MatchDto;
+import com.example.halisahaapplication.entity.Player;
+import com.example.halisahaapplication.service.MatchService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/matches")
+public class MatchController {
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MatchDto> createMatch(MatchDto matchDto){
+        MatchDto savedMatch = matchService.createMatch(matchDto);
+
+        return new ResponseEntity<>(savedMatch, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatchDto> getMatcById(@PathVariable Long id){
+        MatchDto matchDto = matchService.getMatchById(id);
+        return ResponseEntity.ok(matchDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MatchDto>> getAllMatches(){
+        List<MatchDto> matches = matchService.getAllMatchs();
+        return ResponseEntity.ok(matches);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MatchDto> updateMatch(@PathVariable Long id,@RequestBody MatchDto matchDto){
+        MatchDto updatedMatch = matchService.updateMatch(id, matchDto);
+        return ResponseEntity.ok(updatedMatch);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMatch(@PathVariable Long id){
+        matchService.deleteMatch(id);
+        return ResponseEntity.ok("Maç başarıyla silindi");
+    }
+}
---
+++ src/main/java/com/example/halisahaapplication/controller/PlayerController.java
@@ -0,0 +1,52 @@
+package com.example.halisahaapplication.controller;
+
+import com.example.halisahaapplication.dto.PlayerDto;
+import com.example.halisahaapplication.service.PlayerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PlayerDto> createPlayer(PlayerDto playerDto){
+        PlayerDto savedPlayer = playerService.createPlayer(playerDto);
+
+        return new ResponseEntity<>(savedPlayer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlayerDto> getPlayerById(@PathVariable long id){
+        PlayerDto playerDto = playerService.getPlayerById(id);
+
+        return ResponseEntity.ok(playerDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlayerDto>> getAllPlayers(){
+        List<PlayerDto> players = playerService.getAllPLayers();
+        return ResponseEntity.ok(players);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlayerDto> updatePlayer(@PathVariable Long id,@RequestBody PlayerDto playerDto){
+        PlayerDto updatedPlayer = playerService.updatePlayer(id, playerDto);
+        return ResponseEntity.ok(updatedPlayer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePlayer(@PathVariable Long id){
+        playerService.deletePlayer(id);
+        return ResponseEntity.ok("Oyuncu Başarıyla silindi");
+    }
+
+}
